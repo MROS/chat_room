952b63833b07a40a960f3a4289ae7279f8d29021
MROS <yc1043@gmail.com>
2014-11-04 13:08:19 +0800

    member system OK

--- server/src/main/scala/ChatRoomServer.scala
+++ server/src/main/scala/ChatRoomServer.scala
@@ -152,34 +152,75 @@ class HandleRequest(request: ProtocolData, socket: Socket
 
 class HandleClient(socketArray: ArrayBuffer[Socket], socket: Socket) extends Runnable {
 
-  def login: String = {
+  def register(username: String, password: String) = {
+    val out = new PrintWriter(socket.getOutputStream)
+    var connection: java.sql.Connection = null
+    try {
+      Class.forName("com.mysql.jdbc.Driver")
+      connection = java.sql.DriverManager.getConnection(DefaultSetting.database)
+      val statement = connection.createStatement()
+      val affect = statement.executeUpdate(s"""INSERT INTO Members (username, password) VALUES ("${username}", "${password}");""")
+      if (affect == 1) {
+        println(s"$username register successfully")
+        out.println("OK")
+      }
+    }
+    catch {
+      case e: com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException => {
+        println(s"$username register fail")
+        out.println("fail")
+      }
+    }
+    finally {
+      out.flush()
+      connection.close()
+    }
+  }
+  def login(username: String, password: String): Boolean = {
+    val out = new PrintWriter(socket.getOutputStream)
+    Class.forName("com.mysql.jdbc.Driver")
+    val connection = java.sql.DriverManager.getConnection(DefaultSetting.database)
+    val statement = connection.createStatement()
+    val rs = statement.executeQuery(s"""select (password) from Members where username = '$username';""")
+    val realPass = if (!rs.next()) null else rs.getString("password")
+    connection.close()
+    val ans = if (password == realPass) true else false
+    if (ans) out.println("OK") else out.println("fail")
+    out.flush()
+    ans
+  }
+  def LoginOrRegister: ProtocolData = {
     val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
     val requestStr = in.readLine()
     if (requestStr == null) {
       socket.close()
-      ""
+      null
     }
     else {
-      val request = ParseRequest.parse(requestStr)
-      request match {
-        case Login(username, password) => username
-        case _ => {println("function is " + request); ""}
-      }
+      ParseRequest.parse(requestStr)
     }
   }
 
   def run(): Unit = {
-    val username = login
-    if (username != "") println(username + " login")
-    while (!socket.isClosed()) {
-      val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
-      val msg = in.readLine()
-      if (msg == null) {
-        socket.close()
+    LoginOrRegister match {
+      case Register(username, password) => {
+        register(username, password)
       }
-      else {
-        val request: ProtocolData = ParseRequest.parse(msg)
-        (new Thread(new HandleRequest(request, socket, socketArray, username))).start
+      case Login(username, password) => {
+        if (login(username, password) == false) {
+          socket.close()
+        }
+        while (!socket.isClosed()) {
+          val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+          val msg = in.readLine()
+          if (msg == null) {
+            socket.close()
+          }
+          else {
+            val request: ProtocolData = ParseRequest.parse(msg)
+            (new Thread(new HandleRequest(request, socket, socketArray, username))).start
+          }
+        }
       }
     }
     // TODO: 依然有可能別的 HandleClient 用到未移除的socket，所以也必須做處理
